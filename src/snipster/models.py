915b237a580acaf2b09201b62674390552aff4b9
sl2902 <laxmsun@hotmail.com>
2025-12-15 03:23:00 +0530

    apply pre-commit formatting fixes

--- src/snipster/models.py
+++ src/snipster/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from enum import Enum
 
 from loguru import logger
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, UniqueConstraint
 
 from snipster.database_manager import DatabaseManager
 
@@ -21,7 +21,8 @@ class Snippet(SQLModel, table=True):
         default=None, description="Description of the code snippet"
     )
     language: Language = Field(
-        default=Language.PYTHON, description="Enum describing the programming language"
+        default=Language.PYTHON.value,
+        description="Enum describing the programming language",
     )
     tags: str | None = Field(
         default=None, description="Labels to identify the code snippet"
@@ -34,8 +35,29 @@ class Snippet(SQLModel, table=True):
     )
     favorite: bool = Field(default=False, description="Is favourite?")
 
+    __table_args__ = (UniqueConstraint("title", "language"),)
 
-if __name__ == "__main__":
+
+if __name__ == "__main__":  # pragma: no cover
     db_manager = DatabaseManager()
     logger.info(db_manager.select_by_snippet_id(Snippet, 1))
-    logger.info(db_manager.select_all(Snippet))
+    results = db_manager.select_all(Snippet)
+    logger.info(f" Number of records fetched: {len(results)}")
+    logger.info("Testing Insert statement")
+    snippet1 = Snippet(
+        title="Hello World",
+        code="print('Hello, World!')",
+        description="Basic Python hello world",
+        language=Language.PYTHON,
+        tags="beginner, tutorial",
+    )
+
+    snippet2 = Snippet(
+        title="For Loop",
+        code="for i in range(10):\n    print(i)",
+        language=Language.PYTHON,
+        tags="loops, basics",
+    )
+
+    snippets = [snippet1, snippet2]
+    db_manager.insert_records(Snippet, snippets)
