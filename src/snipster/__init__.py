cd6dc1b22fd655d4137f5de1177c0dd53ce39e92
sl2902 <laxmsun@hotmail.com>
2025-12-27 02:27:15 +0530

    define public API for snipster package

---
+++ src/snipster/__init__.py
@@ -0,0 +1,7 @@
+"""Snipster - A code snippet management system."""
+
+from snipster.database_manager import DatabaseManager
+from snipster.models import Language, Snippet
+
+__all__ = ["DatabaseManager", "Language", "Snippet"]
+__version__ = "0.1.0"
