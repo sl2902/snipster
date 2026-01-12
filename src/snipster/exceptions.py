2e947628995638a2c5a6099829a2cf389df43e89
sl2902 <laxmsun@hotmail.com>
2026-01-12 17:01:08 +0530

    update doc string for existing error
    add new exception

--- src/snipster/exceptions.py
+++ src/snipster/exceptions.py
@@ -2,4 +2,12 @@
 
 
 class SnippetNotFoundError(Exception):
+    """Raised when snippet with given ID doesn't exist"""
+
+    pass
+
+
+class RepositoryError(Exception):
+    """Base exception for repository operations"""
+
     pass
