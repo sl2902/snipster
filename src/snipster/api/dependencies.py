e167d277f88628722077440d86e7728da796edbd
sl2902 <laxmsun@hotmail.com>
2026-01-17 00:13:25 +0530

    exclude dependency from unit test coverage

--- src/snipster/api/dependencies.py
+++ src/snipster/api/dependencies.py
@@ -3,6 +3,6 @@
 from fastapi import Request
 
 
-def get_repo(request: Request):
+def get_repo(request: Request):  # pragma: no cover
     """Fetch the repository to serve to the FastAPI endpoint"""
     return request.app.state.repo
