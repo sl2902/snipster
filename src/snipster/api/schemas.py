603cb015e3921ceb5dac735b18a244f110785a48
sl2902 <laxmsun@hotmail.com>
2026-01-21 03:13:09 +0530

    add MessageResponse model for API response
    redefine SnippetCreate schema to exclude  field

--- src/snipster/api/schemas.py
+++ src/snipster/api/schemas.py
@@ -2,13 +2,27 @@
 
 from datetime import datetime
 
-from snipster.models import SnippetBase
+from pydantic import BaseModel, Field
+from sqlmodel import SQLModel
+
+from snipster.models import Language, SnippetBase
 
 
-class SnippetCreate(SnippetBase):
-    """Schema for creating a snippet - only user-provided fields"""
+class SnippetCreate(SQLModel):
+    """Schema for creating a snippet - only user-provided fields; exclude `favorite`"""
 
-    pass
+    title: str = Field(..., description="Title of the snippet")
+    code: str = Field(..., description="Code snippet")
+    description: str | None = Field(
+        default=None, description="Description of the code snippet"
+    )
+    language: Language = Field(
+        default=Language.PYTHON,
+        description="Enum describing the programming language",
+    )
+    tags: str | None = Field(
+        default=None, description="Labels to identify the code snippet"
+    )
 
 
 class SnippetResponse(SnippetBase):
@@ -17,3 +31,9 @@ class SnippetResponse(SnippetBase):
     id: int
     created_at: datetime
     updated_at: datetime
+
+
+class MessageResponse(BaseModel):
+    """Schema for API response - custom messages"""
+
+    message: str = Field(..., description="Message to send to end user")
