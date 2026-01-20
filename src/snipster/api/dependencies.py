a9b9a0dae5c5ee37e0e3de067699e6b3f446c053
sl2902 <laxmsun@hotmail.com>
2026-01-20 23:52:43 +0530

    add return type annotation

--- src/snipster/api/dependencies.py
+++ src/snipster/api/dependencies.py
@@ -2,7 +2,9 @@
 
 from fastapi import Request
 
+from snipster.repositories.repository import SnippetRepository
 
-def get_repo(request: Request):  # pragma: no cover
+
+def get_repo(request: Request) -> SnippetRepository:  # pragma: no cover
     """Fetch the repository to serve to the FastAPI endpoint"""
     return request.app.state.repo
