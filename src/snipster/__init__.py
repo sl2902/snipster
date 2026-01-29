3d439d46fd7865a216d5dfb7c4f621d63aecbbc3
sl2902 <laxmsun@hotmail.com>
2026-01-30 03:06:59 +0530

    add Gist model to public API

--- src/snipster/__init__.py
+++ src/snipster/__init__.py
@@ -1,7 +1,7 @@
 """Snipster - A code snippet management system."""
 
-from snipster.models import Snippet
+from snipster.models import Gist, Snippet
 from snipster.types import Language
 
-__all__ = ["Language", "Snippet"]
+__all__ = ["Language", "Snippet", "Gist"]
 __version__ = "0.1.6"
